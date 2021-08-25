d1fbd6e1269a743e0e481ce0b451054521d8447a
flaxinger <70012548+flaxinger@users.noreply.github.com>
2021-08-25 09:38:40 +0900

    Initial commit

---
+++ src/test/java/com/flaxinger/springlearnudemyfinal/SpringLearnUdemyFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flaxinger.springlearnudemyfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnUdemyFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
