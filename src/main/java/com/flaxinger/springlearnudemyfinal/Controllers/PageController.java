fc4e0324e7862b52410dfa3719b96be01cb9044b
flaxinger <70012548+flaxinger@users.noreply.github.com>
2021-08-28 13:44:58 +0900

    Spring Forms Form post DB insert with form posts Extra formatting of Update Status page

--- src/main/java/com/flaxinger/springlearnudemyfinal/Controllers/PageController.java
+++ src/main/java/com/flaxinger/springlearnudemyfinal/Controllers/PageController.java
@@ -4,6 +4,7 @@ import com.flaxinger.springlearnudemyfinal.model.StatusUpdate;
 import com.flaxinger.springlearnudemyfinal.service.StatusUpdateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,23 +25,27 @@ public class PageController {
     @RequestMapping("/about")
     public String about() { return "app.about"; }
 
+    // @ModelAttribute creates the object + adds it to the model
+    // Is it ok not to use it?
+    //   Spring actually creates a new statusUpdate and adds it to the model automatically
+    //   However, for clarity it is good practice to add it nonetheless
     @RequestMapping(value = "/addstatus", method = RequestMethod.GET)
-    public ModelAndView addStatus(ModelAndView modelAndView) {
+    public ModelAndView addStatus(ModelAndView modelAndView, @ModelAttribute("statusUpdate") StatusUpdate statusUpdate) {
 
         modelAndView.setViewName("app.addStatus");
 //        return "app.addStatus";
-        StatusUpdate statusUpdate = new StatusUpdate();
+//        StatusUpdate statusUpdate = new StatusUpdate();
 
         // Posting most recent status update
         StatusUpdate latestStatusUpdate = statusUpdateService.getLatest();
         modelAndView.getModel().put("latestStatusUpdate", latestStatusUpdate);
 
-        modelAndView.getModel().put("statusUpdate", statusUpdate);
+//        modelAndView.getModel().put("statusUpdate", statusUpdate);
         return modelAndView;
     }
 
     @RequestMapping(value = "/addstatus", method = RequestMethod.POST)
-    public ModelAndView addStatus(ModelAndView modelAndView, StatusUpdate statusUpdate) {
+    public ModelAndView addStatus(ModelAndView modelAndView, StatusUpdate statusUpdate, String temp){
 
         modelAndView.setViewName("app.addStatus");
         statusUpdateService.save(statusUpdate);
@@ -48,6 +53,10 @@ public class PageController {
         // Posting most recent status update
         StatusUpdate latestStatusUpdate = statusUpdateService.getLatest();
         modelAndView.getModel().put("latestStatusUpdate", latestStatusUpdate);
+
+        // Sending new status update to model after update
+        // this the input form after post
+        modelAndView.getModel().put("statusUpdate", new StatusUpdate());
 
         return modelAndView;
     }
